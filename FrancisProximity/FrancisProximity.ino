53436483b1e015603d73b208a65aab37a137fbe7
Mike Everson <mike.everson@climaxmedia.com>
2015-04-09 10:46:52 -0400

    fleshed out eyes and orb logic

--- FrancisProximity/FrancisProximity.ino
+++ FrancisProximity/FrancisProximity.ino
@@ -8,7 +8,7 @@
  This is the logic behind Francis the Fantastic, a fire fortune-teller being created as part of the 2015 Burning Man Honourarium installation, the Charnival.
 *******************************************************************************/
 
-// touch includes 
+// touch includes
 #include <MPR121.h>
 #include <Wire.h>
 
@@ -37,7 +37,7 @@ SdFat sd;
 // LED includes & definitions (pins 10-13 are unallocated)
 #include <Adafruit_NeoPixel.h>
 #define NEOPIXEL 11 // Works on pin 1, but may interfere with serial stuff, try this
-#define EYESTRIP 12 
+#define EYESTRIP 12
 
 // Adafruit_NeoPixel(number of pixels in strip, pin #, pixel type flags add as needed)
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
@@ -47,6 +47,19 @@ SdFat sd;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, NEOPIXEL, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel eyes = Adafruit_NeoPixel(2, EYESTRIP, NEO_GRB + NEO_KHZ800); //todo: make sure we have the right version - might need different params for the eyes depending
 
+// LED tracking values for the orb and the eyes for fading purposes. Everything starts white.
+int stripGreenStart = 255;
+int stripRedStart = 255;
+int stripBlueStart = 255;
+
+int eyesGreenLeftStart = 0;
+int eyesRedLeftStart = 255;
+int eyesBlueLeftStart = 0;
+int eyesGreenRightStart = 0;
+int eyesRedRightStart = 255;
+int eyesBlueRightStart = 0;
+int eyesBrightnessStart = 10;
+
 // Game model variables
 //todo: test time calculations
 bool isRoundActive = false; // Whether or not there is a round in play
@@ -55,13 +68,16 @@ unsigned long roundTimeout = 5000;    // How long to wait after last touch befor
 
 // Get this party started
 void setup() {
+  //todo: Set up the serial communication with the dispenser
   Serial.begin(57600);
   //while (!Serial) ; {} //uncomment when using the serial monitor
-  
+  //todo: command to fix card length (maybe not required?)
+
+
   // Set the output pins
   pinMode(SOLENOID, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
   // SD Card setup
   if (!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
 
@@ -74,8 +90,10 @@ void setup() {
 }
 
 void loop() {
+  //todo: for fading of eyes or what have you, maybe set current state for looping purposes
+
   // If sufficient time has passed since the last touch, enter the completion mode, otherwise continue listening
-  if (isRoundActive && hasRoundTimedOut()){
+  if (isRoundActive && hasRoundTimedOut()) {
     completeRound();
   } else {
     listenForTouchInputs();
@@ -124,7 +142,7 @@ void setupCrystalBall() {
 // LED Eyes setup
 void setupEyes() {
   eyes.begin();
-  resetEyeColor();
+  updateEyeColor(false);
 }
 
 /* UTILITY FUNCTIONS */
@@ -133,162 +151,172 @@ void setupEyes() {
 void listenForTouchInputs() {
   if (MPR121.touchStatusChanged()) {
     MPR121.updateTouchData();
-    
-    //todo: only make an action if we have one or fewer pins touched, or only do the first action
+
     //todo: nyan cat easter egg
 
-    //todo: ignore or handle multi-touch - currently we only react if there is a single touch which can lead to false negatives. 
+    //todo: ignore or handle multi-touch - currently we only react if there is a single touch which can lead to false negatives.
     //      Current behaviour will play sounds for all touched pins. Leave this way for testing purposes, need to calibrate touch sensitivity and crossed wires
     //if (MPR121.getNumTouches() <= 1) {
-      
-      // Iterate through all pins, and for each, check if we have a new touch on it. 
-      //todo: There should be a better way, can't we just get informed of a specific touch?
-      for (int i = firstPin; i <= lastPin; i++) {
-        
-        // If this is a new touch on this pin
-        if (MPR121.isNewTouch(i)) {
-          //pin i was touched          
-          digitalWrite(LED_BUILTIN, HIGH);
-          strip.setBrightness(40);
-          
-          //todo: maybe have francis's eyes open at this point?
-          // Set the round as active and reset the timeout counter
-          isRoundActive = true;
-          resetTimer();
 
-          //todo: instead of stoptrack is there a fade out?
-          //todo: loop sounds while holding instead of play?
-          //todo: don't stop other tracks when touchdown?
+    // Iterate through all pins, and for each, check if we have a new touch on it.
+    //todo: There should be a better way, can't we just get informed of a specific touch?
+    for (int i = firstPin; i <= lastPin; i++) {
 
-          // if we're already playing a track, stop that one and play the newly requested one
-          if (MP3player.isPlaying()) {
-              MP3player.stopTrack();
-              MP3player.playTrack(i - firstPin);
-          } else {
-            MP3player.playTrack(i - firstPin);
-          }
-        } else if (MPR121.isNewRelease(i)) {
-          // Pin i was released
-          digitalWrite(LED_BUILTIN, LOW);
-          strip.setBrightness(10);
+      // If this is a new touch on this pin
+      if (MPR121.isNewTouch(i)) {
+        //pin i was touched
+        digitalWrite(LED_BUILTIN, HIGH);
+        strip.setBrightness(50);
+
+        // Set the round as active and reset the timeout counter
+        isRoundActive = true;
+        resetTimer();
+
+        //todo: instead of stoptrack is there a fade out?
+        //todo: loop sounds while holding instead of play?
+        //todo: don't stop other tracks when touchdown?
+
+        // if we're already playing a track, stop that one and play the newly requested one
+        if (MP3player.isPlaying()) {
+          MP3player.stopTrack();
+          MP3player.playTrack(i - firstPin);
+        } else {
+          MP3player.playTrack(i - firstPin);
         }
+      } else if (MPR121.isNewRelease(i)) {
+        // Pin i was released
+        digitalWrite(LED_BUILTIN, LOW);
+        strip.setBrightness(10);
       }
+    }
     //}
   }
 }
 
 // Handles the round completion
-void completeRound(){
+void completeRound() {
   isRoundActive = false;
-  
+
   // Make eyes change colours
-  updateEyeColor();
+  updateEyeColor(true);
   
-  //todo: make crystal ball change some colours, do something cool
-  
+ // Make crystal ball get very bright on its current color
+  updateOrbBrightness(true);
+
   //todo: maybe play some mp3
-  
+
   // fire the flame effect some number of times
   fireSolenoid();
-  
+
   // dispense a card
   dispenseFortune();
 
-  // delay for a bit before resetting everything
+  // delay for a bit before resetting everything.
   delay(5000);
+  updateOrbBrightness(false);
+  updateEyeColor(false);
   resetTimer();
-  resetEyeColor();
-  //todo: reset eye color, orb color
 }
 
-//todo: keep the orb's color shifting and changing
-void updateOrbColor(){
-  int green;
-  int red;
-  int blue;
-  
-  //todo: some calculation based on millis() to pick color and brightness. Get current seconds (/ 1000?) value to dictate color, millis value dictate brightness?
-  // millis / 1000 % 5 = which color to use, beware there will be some type issues here
-  // millis / 10 % 50 = which brightness to use
-  //todo: test if dividing two longs keeps the result a long
-  unsigned long seconds = 1000;
-  unsigned long colorSequence = (millis() / seconds) % 5; //5 possible colors based on what second we're in
-  //todo: test decreasing once 49 is reached, some sort of negatives function
-  
-  //todo: something like, random 0 to 2 to pick R/G/B and a direction, then while its not 0/255 keep increasing/decreasing, then pick another color
-  
-  switch (colorSequence){
-    case 1:
-      green = 0;
-      red = 255;
-      blue = 0;
-      break;
-    case 2:
-      green = 0;
-      red = 0;
-      blue = 255;
-      break;
-    case 3:
-      green = 127;
-      red = 150;
-      blue = 150;
-      break;
-    case 4:
-      green = 255;
-      red = 255;
-      blue = 255;
-      break;
-    default:
-      green = 255;
-      red = 0;
-      blue = 0;
-      break;
+// Keep the orb's color shifting and changing
+void updateOrbColor() {
+  int Gend = random(0, 255);
+  int Rend = random(0, 255);
+  int Bend = random(0, 255);
+  int rate = 50;
+
+  // Pick a random color and fade to that color
+  for (int i = 0; i < rate; i++) // larger values of 'rate' will give a smoother/slower transition.
+  {
+    int Gnew = stripGreenStart + (Gend - stripGreenStart) * i / rate;
+    int Rnew = stripRedStart + (Rend - stripRedStart) * i / rate;
+    int Bnew = stripBlueStart + (Bend - stripBlueStart) * i / rate;
+
+    // set pixel color here
+    for (int i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Gnew, Rnew, Bnew);
+    }
   }
 
-  for (int i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, green, red, blue);
-  }
+  stripGreenStart = Gend;
+  stripRedStart = Rend;
+  stripBlueStart = Bend;
 
   strip.show();
 }
 
-// eyes glow pattern
-void updateEyeColor(){
-  int eyeSequence =  random(0, 2);
-  
-  //todo: write a function to generate sequences. randomize brightness, randomize color, always fade
-  switch (eyeSequence) {
-    case 1:
-      eyes.setBrightness(25);
-    
-      for (int i = 0; i < strip.numPixels(); i++) {
-        eyes.setPixelColor(i, 255, 0, 0);
-      }      
-      break;
-    case 2:
-      eyes.setBrightness(25);
-    
-      for (int i = 0; i < strip.numPixels(); i++) {
-        eyes.setPixelColor(i, 0, 0, 255);
-      }       
-      break;
-    default:
-      //todo: pulse to a bright red
-      eyes.setBrightness(50);
-    
-      for (int i = 0; i < strip.numPixels(); i++) {
-        eyes.setPixelColor(i, 0, 255, 0);
-      }
-      break;
+// Brightens or darkens the orb
+void updateOrbBrightness(bool isThinkingState) {
+  int brightnessStart = 50;
+  int brightnessEnd = 10;
+  int rate = 50;
+
+  if (isThinkingState) {
+    brightnessStart = 10;
+    brightnessEnd = 50;
   }
 
-  eyes.show();
+  // Larger values of 'rate' will give a smoother/slower transition.
+  for (int i = 0; i < rate; i++)
+  {
+    strip.setBrightness(brightnessStart + (brightnessEnd - brightnessStart) * i / rate);
+  }
+}
+
+// Eyes glow pattern
+void updateEyeColor(bool isThinkingState) {
+  // Pick a color for each eye and slowly fade them both, along with brightness
+  int GLeftEnd = 0;
+  int RLeftEnd = 255;
+  int BLeftEnd = 0;
+  int GRightEnd = 0;
+  int RRightEnd = 255;
+  int BRightEnd = 0;
+  int brightnessStart = 50;
+  int brightnessEnd = 10;
+  int rate = 10;
+
+  if (isThinkingState) {
+    GLeftEnd = random(0, 255);
+    RLeftEnd = random(0, 255);
+    BLeftEnd = random(0, 255);
+    GRightEnd = random(0, 255);
+    RRightEnd = random(0, 255);
+    BRightEnd = random(0, 255);
+    brightnessStart = 10;
+    brightnessEnd = 50;
+  }
+
+  // Larger values of 'rate' will give a smoother/slower transition.
+  for (int i = 0; i < rate; i++)
+  {
+    int GLeftNew = eyesGreenLeftStart + (GLeftEnd - eyesGreenLeftStart) * i / rate;
+    int RLeftNew = eyesRedLeftStart + (RLeftEnd - eyesRedLeftStart) * i / rate;
+    int BLeftNew = eyesBlueLeftStart + (BLeftEnd - eyesBlueLeftStart) * i / rate;
+    int GRightNew = eyesGreenRightStart + (GRightEnd - eyesGreenRightStart) * i / rate;
+    int RRightNew = eyesRedRightStart + (RRightEnd - eyesRedRightStart) * i / rate;
+    int BRightNew = eyesBlueRightStart + (BRightEnd - eyesBlueRightStart) * i / rate;
+
+    // set pixel color & brightness
+    eyes.setPixelColor(0, GLeftNew, RLeftNew, BLeftNew);
+    eyes.setPixelColor(1, GRightNew, RRightNew, BRightNew);
+    eyes.setBrightness(brightnessStart + (brightnessEnd - brightnessStart) * i / rate);
+  }
+
+  eyesGreenLeftStart = GLeftEnd;
+  eyesRedLeftStart = RLeftEnd;
+  eyesBlueLeftStart = BLeftEnd;
+  eyesGreenLeftStart = GLeftEnd;
+  eyesRedLeftStart = RLeftEnd;
+  eyesBlueLeftStart = BLeftEnd;
+
+  strip.show();
 }
 
 // Fire the flame effect
 void fireSolenoid() {
   int firingSequence =  random(0, 2);
-  
+
   //todo: write a function to generate sequences. randomize delay up to a max, randomize number of shots to a max
   switch (firingSequence) {
     case 1:
@@ -298,7 +326,7 @@ void fireSolenoid() {
       delay(500);
       digitalWrite(SOLENOID, HIGH);
       delay(500);
-      digitalWrite(SOLENOID, LOW);      
+      digitalWrite(SOLENOID, LOW);
       break;
     case 2:
       digitalWrite(SOLENOID, HIGH);
@@ -307,7 +335,7 @@ void fireSolenoid() {
       delay(1000);
       digitalWrite(SOLENOID, HIGH);
       delay(1000);
-      digitalWrite(SOLENOID, LOW);  
+      digitalWrite(SOLENOID, LOW);
       break;
     default:
       digitalWrite(SOLENOID, HIGH);
@@ -317,27 +345,24 @@ void fireSolenoid() {
   }
 }
 
-void dispenseFortune(){
-  //todo: weird serial magic to trigger the card dispenser 
-  //todo: some sort of error handling. make orb turn red or play a sound if out of cards or something?
+void dispenseFortune() {
+  //todo: weird serial magic to trigger the card dispenser
+
+  //todo: something like this...
+  //   Issuing Command
+  //   Status request command
+  //    If busy flag, keep polling status
+  //    If error bit, do some display warning like play sound/orb red/something else
+  //   Clear Command
 }
 
-bool hasRoundTimedOut(){
+bool hasRoundTimedOut() {
   return  millis() - baseTime > roundTimeout;
 }
 
 // Set the baseline value for the timer
-void resetTimer(){
+void resetTimer() {
   baseTime = millis();
 }
 
-void resetEyeColor(){
-  eyes.setBrightness(10);
-
-  for (int i = 0; i < eyes.numPixels(); i++) {
-    eyes.setPixelColor(i, 0, 0, 0);
-  }
-
-  eyes.show();
-}
 
