33114bcb3c88a355e28014e5b72b76ee285b1483
Mike Everson <everson.mike@gmail.com>
2016-08-30 16:53:29 -0400

    Refactors scale code to be a bit cleaner

--- Scale_Output/Scale_Output.ino
+++ Scale_Output/Scale_Output.ino
@@ -1,122 +1,87 @@
-/*
- Example using the SparkFun HX711 breakout board with a scale
- By: Nathan Seidle
- SparkFun Electronics
- Date: November 19th, 2014
- License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
- 
- This example demonstrates basic scale output. See the calibration sketch to get the calibration_factor for your
- specific load cell setup.
- 
- This example code uses bogde's excellent library: https://github.com/bogde/HX711
- bogde's library is released under a GNU GENERAL PUBLIC LICENSE
- 
- The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
- based load cell which should allow a user to measure everything from a few grams to tens of tons.
- Arduino pin 2 -> HX711 CLK
- 3 -> DAT
- 5V -> VCC
- GND -> GND
- 
- The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
- 
-*/
-
 #include "HX711.h"
 #include <Adafruit_NeoPixel.h>
 
-#define calibration_factor -9330.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+//This value is obtained using the SparkFun_HX711_Calibration sketch. Currently calibrated to lbs
+#define calibration_factor -9330.0 
 
 #define DOUT  3
 #define CLK  2
 #define LED_PIN 4
 
 HX711 scale(DOUT, CLK);
-int N_PIXELS = 24;
-int kegWeightFull = 130;
-int kegWeightEmpty = 10;
-int updateFrequency = 1;
+int totalPixels = 24; // LEDs in the neopixel ring
+int kegWeightFull = 130; //in lbs, which is what the scale is calibrated to
+int updateFrequency = 1; 
+int dimFactor = 2; // keeps the ratios but dims the brightness of the LED ring
+int colorTiers = 9; // Determines which rgbVal to display based on the weight
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+int rgbVals[10][3] = {
+  { 255, 0, 0 }, // Empty, red
+  { 255, 16, 0},
+  { 255, 32, 0 },
+  { 255, 64, 0 },
+  { 255, 128, 0 },
+  { 255, 255, 0 },
+  { 128, 255, 0 },
+  { 64, 255, 0 },
+  { 32, 255, 0 },
+  { 0, 255, 0 }, // Full, green
+};
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalPixels, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 scale demo");
 
-  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  scale.set_scale(calibration_factor); 
+
+  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  scale.tare();  
 
   strip.begin();
-  Serial.println("Readings:");
 }
 
 void loop() {
   Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
-  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  
+  //scale.get_units() returns a float
+  Serial.print(scale.get_units(), 1); 
+  
+   //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.print(" lbs");
   Serial.println();
   
   setColor();
 }
 
 void setColor(){
-  float weight = scale.get_units();
-  float percentage = weight / kegWeightFull;
-  
   int red = 0;
   int green = 0;
   int blue = 0;
   
-  int pixels = percentage * 24;
-  int percentile = percentage / 0.125;
-  
-  Serial.print("Percentile: ");
-  Serial.print(percentile);
-  Serial.println();
-  Serial.print("Pixels: ");
-  Serial.print(pixels);
-  Serial.println();
-  
-  if (percentile == 0){
-    red = 255;
-    green = 0;
-  } else if (percentile == 1){
-    red = 255;
-    green = 16;
-  } else if (percentile == 2){
-    red = 255;
-    green = 32;
-  } else if (percentile == 3){
-    red = 255;
-    green = 64;
-  } else if (percentile == 4){
-    red = 255;
-    green = 128;
-  } else if (percentile == 5){
-    red = 255;
-    green = 255;
-  } else if (percentile == 6){
-    red = 128;
-    green = 255;
-  } else if (percentile == 7){
-    red = 64;
-    green = 255;
-  } else if (percentile == 8){
-    red = 32;
-    green = 255;
-  } else if (percentile > 8){
-    red = 0;
-    green = 255;
-  } else{
-    red = 255;
-    green = 0;
+  float weight = scale.get_units();
+  float percentage = weight / kegWeightFull;
+
+  // Normalize anything over 100% to 100%
+  if (percentage > 1){
+    percentage = 1;
   }
+
+  // There are eight tiers of color
+  int activePixels = percentage * totalPixels;
+  int colorTier = percentage / (1 / colorTiers);
+
+  // Pick the color based on how full the keg is
+  red = rgbVals[colorTier][0];
+  green = rgbVals[colorTier][1];
+  blue = rgbVals[colorTier][2];
   
-  for(int i=0; i<N_PIXELS; i++) {
-    if ((i + 1) > pixels && i > 0){ 
+  // Apply the color settings
+  for(int i=0; i<totalPixels; i++) {
+    if (i > 0 && i >= activePixels){ 
       strip.setPixelColor(i, 0, 0, 0);
     } else {
-      strip.setPixelColor(i, red / 2, green / 2, blue / 2);
+      strip.setPixelColor(i, red / dimFactor, green / dimFactor, blue / dimFactor);
     }
   }
   
