57048814984dc832851a500de9fe32e1c17dcab7
Mike Everson <everson.mike@gmail.com>
2017-04-16 00:29:31 -0400

    More integration test code

--- Altar/altar.ino
+++ Altar/altar.ino
@@ -13,7 +13,6 @@
 #define LED_SAFETY_ALTAR A3
 #define LED_SAFETY_TABLE A4
 
-// Getting weird behaviour of some of the other pins like 0 and 8
 #define GO_BUTTON 7
 #define SPELL_BUTTON 8
 
@@ -27,10 +26,14 @@
 
 HX711 scale(DOUT, CLK);
 
-//int totalPixels = 24; // LEDs in the neopixel ring
-//int kegWeightFull = 130; //in lbs, which is what the scale is calibrated to
-//int dimFactor = 2; // keeps the ratios but dims the brightness of the LED ring
-//float colorTiers = 10; // Helps which rgbVal to display based on the weight, needs to be float for division purposes
+// Adafruit_NeoPixel(number of pixels in strip, pin #, pixel type flags add as needed)
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//Adafruit_NeoPixel altar_ready = Adafruit_NeoPixel(180, LED_ALTAR_READY, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel altar_safety = Adafruit_NeoPixel(150, LED_SAFETY_ALTAR, NEO_GRB + NEO_KHZ800); 
+Adafruit_NeoPixel table_safety = Adafruit_NeoPixel(300, LED_SAFETY_TABLE, NEO_GRB + NEO_KHZ800); 
 
 
 //Adafruit_NeoPixel strip = Adafruit_NeoPixel(totalPixels, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -38,9 +41,9 @@ HX711 scale(DOUT, CLK);
 void setup() {
   Serial.begin(9600);
 
-  pinMode(LED_ALTAR_READY, OUTPUT);     
-  pinMode(LED_SAFETY_ALTAR, OUTPUT);      
-  pinMode(LED_SAFETY_TABLE, OUTPUT);
+//  pinMode(LED_ALTAR_READY, OUTPUT);     
+//  pinMode(LED_SAFETY_ALTAR, OUTPUT);      
+//  pinMode(LED_SAFETY_TABLE, OUTPUT);
 pinMode(GO_BUTTON, INPUT_PULLUP);      
   pinMode(SPELL_BUTTON, INPUT_PULLUP); 
 pinMode(2, OUTPUT);  
@@ -57,10 +60,33 @@ digitalWrite(4, HIGH);
   //Assuming there is no weight on the scale at start up, reset the scale to 0
   scale.tare();  
 
-//  strip.begin();
+//altar_ready.begin();
+//altar_safety.begin();
+table_safety.begin();
+
+//altar_ready.show();
+//altar_safety.show();
+table_safety.show();
 }
 
 void loop() {
+  for(int i = 0; i < 300; i++){
+    if (i < 150){
+//      altar_safety.setPixelColor(i, 0, 0, 0);
+ 
+    }
+    if (i < 180){
+//           altar_ready.setPixelColor(i, 0, 0, 125);
+    }
+    
+    
+      table_safety.setPixelColor(i, 0, 0, 0);
+  }
+
+//altar_ready.show();
+//altar_safety.show();
+table_safety.show();
+  
   //scale.get_units() returns a float
   Serial.print(scale.get_units(), 1); 
   
