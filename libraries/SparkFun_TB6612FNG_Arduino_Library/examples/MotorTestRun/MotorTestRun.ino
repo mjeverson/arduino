534d2826dc08adfa976ebe22342183b31fdb409d
mjeverson <everson.mike@gmail.com>
2018-08-12 15:20:59 -0400

    picks pins for motors, tests back to back running, need to try threading/parallel

--- libraries/SparkFun_TB6612FNG_Arduino_Library/examples/MotorTestRun/MotorTestRun.ino
+++ libraries/SparkFun_TB6612FNG_Arduino_Library/examples/MotorTestRun/MotorTestRun.ino
@@ -4,15 +4,12 @@ TB6612FNG H-Bridge Motor Driver Example code
 Michelle @ SparkFun Electronics
 8/20/16
 https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
-
 Uses 2 motors to show examples of the functions in the library.  This causes
 a robot to do a little 'jig'.  Each movement has an equal and opposite movement
 so assuming your motors are balanced the bot should end up at the same place it
 started.
-
 Resources:
 TB6612 SparkFun Library
-
 Development environment specifics:
 Developed on Arduino 1.6.4
 Developed with ROB-9457
@@ -26,12 +23,17 @@ Developed with ROB-9457
 // the default pins listed are the ones used on the Redbot (ROB-12097) with
 // the exception of STBY which the Redbot controls with a physical switch
 #define AIN1 2
-#define BIN1 7
-#define AIN2 4
+#define BIN1 7 //todo: why does setting these to 23-25 cause problems?
+#define CIN1 12 //todo: why does setting these to 23-25 cause problems?
+#define AIN2 3
 #define BIN2 8
-#define PWMA 5
-#define PWMB 6
-#define STBY 9
+#define CIN2 13
+#define PWMA 4
+#define PWMB 5
+#define PWMC 6
+#define STBYA 9
+#define STBYB 10
+#define STBYC 11
 
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
@@ -42,12 +44,13 @@ const int offsetB = 1;
 // motors as you have memory for.  If you are using functions like forward
 // that take 2 motors as arguements you can either write new functions or
 // call the function more than once.
-Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
-Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBYA);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBYB);
 
 void setup()
 {
  //Nothing here
+ Serial.begin(9600);
 }
 
 
@@ -57,47 +60,62 @@ void loop()
    //and optional duration.  A negative speed will cause it to go
    //backwards.  Speed can be from -255 to 255.  Also use of the 
    //brake function which takes no arguements.
-//   motor1.drive(255,1000);
-//   motor1.drive(-255,1000);
-//   motor1.brake();
+   
+   
+   //Use of the drive function which takes as arguements the speed
+   //and optional duration.  A negative speed will cause it to go
+   //backwards.  Speed can be from -255 to 255.  Also use of the 
+   //brake function which takes no arguements.
+   Serial.println("Doing motor2 drive pos");
+   motor2.drive(255,1000);
+   Serial.println("Doing motor2 drive neg");
+   motor2.drive(-255,1000);
+   Serial.println("Doing motor2 break");
+   motor2.brake();
+   delay(1000);
+
+Serial.println("Doing motor1 drive pos");
+   motor1.drive(255,1000);
+   Serial.println("Doing motor1 drive neg");
+   motor1.drive(-255,1000);
+   Serial.println("Doing motor1 break");
+   motor1.brake();
+   delay(1000);
+   
+//   //Use of the forward function, which takes as arguements two motors
+//   //and optionally a speed.  If a negative number is used for speed
+//   //it will go backwards
+//   Serial.println("Doing motor1-2 forward pos");
+//   forward(motor1, motor2, 150);
 //   delay(1000);
 //   
-//   //Use of the drive function which takes as arguements the speed
-//   //and optional duration.  A negative speed will cause it to go
-//   //backwards.  Speed can be from -255 to 255.  Also use of the 
-//   //brake function which takes no arguements.
-//   motor2.drive(255,1000);
-//   motor2.drive(-255,1000);
-//   motor2.brake();
+//   //Use of the back function, which takes as arguments two motors 
+//   //and optionally a speed.  Either a positive number or a negative
+//   //number for speed will cause it to go backwards
+//   Serial.println("Doing motor1-2 back neg");
+//   back(motor1, motor2, -150);
 //   delay(1000);
-   
-   //Use of the forward function, which takes as arguements two motors
-   //and optionally a speed.  If a negative number is used for speed
-   //it will go backwards
-//   forward(motor1, motor2, -150);
-//   delay(1000);
-   
-   //Use of the back function, which takes as arguments two motors 
-   //and optionally a speed.  Either a positive number or a negative
-   //number for speed will cause it to go backwards
-//   back(motor1, motor2, -1500);
-//   delay(1000);
-   
-   //Use of the brake function which takes as arguments two motors.
+//   
+//   //Use of the brake function which takes as arguments two motors.
 //   //Note that functions do not stop motors on their own.
+//   Serial.println("Doing motor1-2 brake");
 //   brake(motor1, motor2);
 //   delay(1000);
 //   
 //   //Use of the left and right functions which take as arguements two
 //   //motors and a speed.  This function turns both motors to move in 
 //   //the appropriate direction.  For turning a single motor use drive.
+//   Serial.println("Doing motor1-2 left");
 //   left(motor1, motor2, 100);
 //   delay(1000);
+//   Serial.println("Doing motor1-2 right");
 //   right(motor1, motor2, 100);
 //   delay(1000);
 //   
 //   //Use of brake again.
+//   Serial.println("Doing motor1-2 brake");
 //   brake(motor1, motor2);
 //   delay(1000);
-//   
+   
 }
+
--- weareallstardust/weareallstardust.ino
+++ weareallstardust/weareallstardust.ino
@@ -17,26 +17,15 @@
 #define LED_RING_A 10
 #define LED_RING_B 11
 #define LED_RING_C 12
-#define ALTAR_PIN 7
+#define LED_ALTAR 7
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip_a = Adafruit_NeoPixel(LED_RING_PIXELS, LED_RING_A, NEO_GRB + NEO_KHZ800);        
 Adafruit_NeoPixel strip_b = Adafruit_NeoPixel(LED_RING_PIXELS, LED_RING_B, NEO_GRB + NEO_KHZ800);        
 Adafruit_NeoPixel strip_c = Adafruit_NeoPixel(LED_RING_PIXELS, LED_RING_C, NEO_GRB + NEO_KHZ800);  
-Adafruit_NeoPixel strip_altar = Adafruit_NeoPixel(180, ALTAR_PIN, NEO_GRB + NEO_KHZ800);      
+Adafruit_NeoPixel strip_altar = Adafruit_NeoPixel(180, LED_ALTAR, NEO_GRB + NEO_KHZ800);      
  
 #define FADE_SPEED 5
 #define PULSE_SPEED 8
-//float readyColors[3] = {127, 127, 127};
-//float globalFadeLevel = 255;
-//bool globalFadeIn;
 
 // Pins the scale is using. Note, we're not actually using CLK for the clock pin, which is odd. Does it only work because of the crystal on the dmxfire?
 //todo: double check these pins since we're using a mega now
@@ -48,27 +37,30 @@ Adafruit_NeoPixel strip_altar = Adafruit_NeoPixel(180, ALTAR_PIN, NEO_GRB + NEO_
 #define calibration_factor -9890.0
 HX711 scale(DOUT, CLK);
 
-//todo: motor values
 // Pins for all inputs, keep in mind the PWM defines must be on PWM pins
+// the default pins listed are the ones used on the Redbot (ROB-12097) with
+// the exception of STBY which the Redbot controls with a physical switch
 #define AIN1 2
-#define BIN1 7
-#define CIN1 3
+#define AIN2 3
+#define PWMA 4
 
-#define AIN2 4
+#define BIN1 7 //todo: why does setting these to 23-25 cause problems?
 #define BIN2 8
-#define CIN2 10
+#define PWMB 5
 
-#define PWMA 5
-#define PWMB 6
-#define PWMC 11
+#define CIN1 12 //todo: why does setting these to 23-25 cause problems?
+#define CIN2 13
+#define PWMC 6
 
-#define STBY 9
+#define STBYA 9
+#define STBYB 10
+#define STBYC 11
 
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
-const int offsetA = 1;
-Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
-Motor motor2 = Motor(7, 8, 6, 1, STBY);
+Motor motor_a = Motor(AIN1, AIN2, PWMA, 1, STBYA);
+Motor motor_b = Motor(BIN1, BIN2, PWMB, 1, STBYB);
+//Motor motor_c = Motor(BIN1, BIN2, PWMB, 1, STBYB);
 
 
 ///todo: new thread class for motors or lights
@@ -99,8 +91,6 @@ void setup() {
   scale.set_scale(calibration_factor); 
   scale.tare();  
 
-//  ledsetup();  
-
   analog1.pin = A1;
   analog1.setInterval(100);
 
@@ -117,8 +107,9 @@ int pulseThreadId;
 int theatreThreadId;
 
 void loop() {
-  theaterChaseStrandTest(strip_altar.Color(127, 127, 127), 50); // White
-//  doAltarReadyPulseThread
+  //todo: keep this it works
+//  theaterChaseStrandTest(strip_altar.Color(127, 127, 127), 50); // White
+
   // Debug
 //  Serial.print(scale.get_units());
 //  Serial.println();
@@ -127,25 +118,41 @@ void loop() {
     // check if a number was received
     if (Serial.read() == '0') {
       Serial.println("IT'S PARTY TIME!");
-//      forward(motor1, motor2, -150);
-      motor1.drive(-150);
+      //todo: why does motor_a not run until after motor_b.brake has been called?
+      //todo: will need to test how this all works with threads...
+      Serial.println("Doing motor2 drive pos");
+      motor_b.drive(255,1000);
+      Serial.println("Doing motor2 drive neg");
+      motor_b.drive(-255,1000);
+      Serial.println("Doing motor2 break");
+      motor_b.brake();
+      delay(1000);
+    
+      Serial.println("Doing motor1 drive pos");
+      motor_a.drive(255,1000);
+      Serial.println("Doing motor1 drive neg");
+      motor_a.drive(-255,1000);
+      Serial.println("Doing motor1 break");
+      motor_a.brake();
+      delay(1000);
 
       analog1.run();
       analog2.run();
     }
     else {
       Serial.println("Party's over... disengaging motor locomotion protocol :(");
-//      forward(motor1, motor2, 150);
-      motor1.drive(150);
+      forward(motor_a, motor_b, 150);
+//      motor_a.drive(150);
     }
   } 
 
   // Get the fresh readings
-  Serial.print("Analog1 Thread: ");
-  Serial.println(analog1.value);
-
-  Serial.print("Analog2 Thread: ");
-  Serial.println(analog2.value);
+  //todo: keep this it seems to work
+//  Serial.print("Analog1 Thread: ");
+//  Serial.println(analog1.value);
+//
+//  Serial.print("Analog2 Thread: ");
+//  Serial.println(analog2.value);
   //todo: need a way to stop threads from running. threadcontroller.clear()?
  
   //while we're looping. if someone's on the scale, do the theater chase. if someone's not on the scale, do the ready pulse.
@@ -163,13 +170,6 @@ void loop() {
     //doAltarReadyPulse();  
   }
 }
-
-//TODO: Might want to do this in a thread while someone is standing on the scale
-// Sets the altar lights in a pattern that indicates it's listening, 
-//void setAltarListenLighting(){
-//  // Theatre lighting around the outside three times
-//  theaterChaseThread(readyColors[0], readyColors[1], readyColors[2], 50);
-//}
 
 //void doAltarReadyPulseThread(){
 //  if (globalFadeLevel >= 255) {
@@ -205,159 +205,3 @@ void theaterChaseStrandTest(uint32_t c, uint8_t wait) {
     }
   }
 }
-//
-//// Theatre-style crawling lights. Changes spacing to be dynamic based on string size
-//void theaterChaseThread(unsigned char r, unsigned char g, unsigned char b, unsigned char wait) {
-//  int theatre_spacing = ALTAR_READY_PIXELS / 10;
-//  
-//  for (int q = 0; q < theatre_spacing ; q++) {   
-//    unsigned int step = 0;
-//    
-//    cli();
-//    
-//    for (int i=0; i < ALTAR_READY_PIXELS ; i++) {
-//      if (step == q) {
-//        sendPixel( r , g , b);
-//      } else {
-//        sendPixel( 0 , 0 , 0);
-//      }
-//      
-//      step++;
-//      
-//      if (step == theatre_spacing) step = 0;
-//    }
-//    
-//    sei();
-//    show();
-//    //todo: probably can't do delay here
-//    delay(wait); 
-//  } 
-//}
-
-// Display a single color on the whole string
-//void showColor(unsigned char r, unsigned char g, unsigned char b) {
-//  cli();  
-//  
-//  for(int p = 0; p < ALTAR_READY_PIXELS; p++) {
-//    sendPixel(r, g, b);
-//  }
-//  
-//  sei();
-//  show();
-//}
-
-
-// **************************************************************************
-// LED API
-// **************************************************************************
-
-// More info on how to find these at http://www.arduino.cc/en/Reference/PortManipulation
-//todo: will need to figure this out again for the mega. Seriously though with the mega can probably just get away with regular neopixel stuff.
-//#define PIXEL_PORT  PORTC  // Port of the pin the pixels are connected to
-//#define PIXEL_DDR   DDRC   // Port of the pin the pixels are connected to
-//
-//// These are the timing constraints taken mostly from the WS2812 datasheets 
-//// These are chosen to be conservative and avoid problems rather than for maximum throughput 
-//#define T1H  900    // Width of a 1 bit in ns
-//#define T1L  600    // Width of a 1 bit in ns
-//#define T0H  400    // Width of a 0 bit in ns
-//#define T0L  900    // Width of a 0 bit in ns
-//#define RES 6000    // Width of the low gap between bits to cause a frame to latch
-//
-//// Here are some convience defines for using nanoseconds specs to generate actual CPU delays
-//// Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
-//#define NS_PER_SEC (1000000000L)          
-//#define CYCLES_PER_SEC (F_CPU)
-//#define NS_PER_CYCLE ( NS_PER_SEC / CYCLES_PER_SEC )
-//#define NS_TO_CYCLES(n) ( (n) / NS_PER_CYCLE )
-//
-//// Actually send a bit to the string. We must to drop to asm to ensure that the compiler does
-//// not reorder things and make it so the delay happens in the wrong place.
-//inline void sendAltarReadyBit(bool bitVal) {
-//  if (bitVal) {        
-//    // 0 bit
-//    asm volatile (
-//      "sbi %[port], %[bit] \n\t"        // Set the output bit
-//      ".rept %[onCycles] \n\t"          // Execute NOPs to delay exactly the specified number of cycles
-//      "nop \n\t"
-//      ".endr \n\t"
-//      "cbi %[port], %[bit] \n\t"        // Clear the output bit
-//      ".rept %[offCycles] \n\t"         // Execute NOPs to delay exactly the specified number of cycles
-//      "nop \n\t"
-//      ".endr \n\t"
-//      ::
-//      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
-//      [bit]   "I" (ALTAR_READY_PIXEL_BIT),
-//      [onCycles]  "I" (NS_TO_CYCLES(T1H) - 2),    // 1-bit width less overhead  for the actual bit setting, note that this delay could be longer and everything would still work
-//      [offCycles]   "I" (NS_TO_CYCLES(T1L) - 2)   // Minimum interbit delay. Note that we probably don't need this at all since the loop overhead will be enough, but here for correctness
-//    );
-//                                  
-//  } else {          
-//    // 1 bit
-//    
-//    // **************************************************************************
-//    // This line is really the only tight goldilocks timing in the whole program!
-//    // **************************************************************************
-//    asm volatile (
-//      "sbi %[port], %[bit] \n\t"      // Set the output bit
-//      ".rept %[onCycles] \n\t"        // Now timing actually matters. The 0-bit must be long enough to be detected but not too long or it will be a 1-bit
-//      "nop \n\t"                      // Execute NOPs to delay exactly the specified number of cycles
-//      ".endr \n\t"
-//      "cbi %[port], %[bit] \n\t"      // Clear the output bit
-//      ".rept %[offCycles] \n\t"       // Execute NOPs to delay exactly the specified number of cycles
-//      "nop \n\t"
-//      ".endr \n\t"
-//      ::
-//      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
-//      [bit]   "I" (ALTAR_READY_PIXEL_BIT),
-//      [onCycles]  "I" (NS_TO_CYCLES(T0H) - 2),
-//      [offCycles] "I" (NS_TO_CYCLES(T0L) - 2)
-//
-//    );
-//      
-//  }
-//  
-//  // Note that the inter-bit gap can be as long as you want as long as it doesn't exceed the 5us reset timeout (which is A long time)
-//  // Here I have been generous and not tried to squeeze the gap tight but instead erred on the side of lots of extra time.
-//  // This has thenice side effect of avoid glitches on very long strings becuase 
-//}  
-//
-//inline void sendByte( unsigned char byte) {
-//    //TODO: There has to be a better way than having a different function for each pixel_bit
-//    //TODO: Get a weird "impossible constraint in 'asm'" error 
-//    for( unsigned char bit = 0 ; bit < 8 ; bit++ ) {
-//        sendAltarReadyBit(bitRead(byte, 7));
-//
-//      // Neopixel wants bit in highest-to-lowest order, so send highest bit (bit #7 in an 8-bit byte since they start at 0)                                                                                 
-//      // and then shift left so bit 6 moves into 7, 5 moves into 6, etc
-//      byte <<= 1;                                   
-//    }           
-//} 
-//
-///*
-//  The following three functions are the public API:
-//  
-//  ledSetup() - set up the pin that is connected to the string. Call once at the begining of the program.  
-//  sendPixel( r g , b ) - send a single pixel to the string. Call this once for each pixel in a frame.
-//  show() - show the recently sent pixel on the LEDs . Call once per frame. 
-//*/
-//
-//void ledsetup() {
-//  // Set the specified pin up as digital out
-//  // https://www.arduino.cc/en/Reference/bitSet
-//  bitSet(PIXEL_DDR, ALTAR_READY_PIXEL_BIT);
-//}
-//
-//inline void sendPixel(unsigned char r, unsigned char g, unsigned char b)  {  
-//  // Neopixel wants colors in green then red then blue order
-//  sendByte(g);          
-//  sendByte(r);
-//  sendByte(b); 
-//}
-//
-//void show() {
-//  // Just wait long enough without sending any bits to cause the pixels to latch and display the last sent frame
-//  // Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
-//  _delay_us( (RES / 1000UL) + 1);       
-//}
-
